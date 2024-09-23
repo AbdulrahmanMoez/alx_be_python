ced44d3de6e8a3bedd0970a4c0ba5c17667bee28
Abdulrahman Moez <165219852+AbdulrahmanMoez@users.noreply.github.com>
2024-09-23 22:38:00 +0300

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,8 +1,6 @@
-# daily_reminder.py
-
-task = input("Enter your task: ")
-priority = input("Priority (high/medium/low): ").lower()
-time_bound = input("Is it time-bound? (yes/no): ").lower()
+task = str(input("Enter your task: "))
+priority = str(input("priority (high, medium, low): ")).lower()
+time_bound = str(input("Is it time-bound? (yes/no): ")).lower()
 
 while task:
     match priority:
@@ -13,14 +11,14 @@ while task:
                 print(f"Reminder: '{task}' is a high priority task that requires immediate attention as soon as possible!")
         case "medium":
             if time_bound == "yes":
-                print(f"Reminder: '{task}' is a medium priority task that should be completed soon.")
+                print(f"Reminder: '{task}' is a medium priority task that requires to be attention to in the next few days")
             else:
-                print(f"Reminder: '{task}' is a medium priority task that can be done in the near future.")
+                print(f"Reminder: '{task}' is a medium priority task that requires to be attention to  in the nearest time")
         case "low":
             if time_bound == "yes":
                 print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
             else:
                 print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
-        case _:
-            print("Invalid priority level entered.")
+        case _ : 
+            print("We don't have any suggestion for this task")
     break
