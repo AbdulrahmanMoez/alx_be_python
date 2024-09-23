7fe3112eb650c9d9abf49312ec08b26f17a0dec9
Abdulrahman Moez <165219852+AbdulrahmanMoez@users.noreply.github.com>
2024-09-23 15:41:43 +0300

    Update pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,7 @@
-square_size = 1
-
-while square_size >= 1:
-    square_size = int(input("Enter the size of the pattern: "))
-    for num in range(square_size):
-        print( "****", end="")
-        print()
+size = int(input("Enter the size of the pattern:"))
+row = 0
+while row < size:
+    for col in range (size):
+        print("*", end="")
+    print()
+    row+=1  
