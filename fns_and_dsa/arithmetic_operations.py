de4d5aacfe707a9688d7467a9e4e5edc276ed14f
Abdulrahman Moez <165219852+AbdulrahmanMoez@users.noreply.github.com>
2024-10-02 16:48:25 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,8 @@ def perform_operation(num1, num2, operation):
     elif operation == "subtract":
         return num1 - num2
     elif operation == "divide":
-        if num2 != 0:
-            return num1 / num2
-        else:
+        if num2 == 0:
             return "Error: Division by Zero is not allowed"
+        return num1 / num2
     else:
         return "Invalid Input"
