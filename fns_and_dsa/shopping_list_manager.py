744336b4a2903b9aca19b6860b05b18a360f1bb6
Abdulrahman <Abdullrahmanmoez@gmail.com>
2024-10-02 07:48:42 -0700

    task 2 for week 5

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,34 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            add = input("Please Add An Item: ")
+            shopping_list.append(add)
+            print(f"{add} Has Been Added! ")
+            pass
+        elif choice == '2':
+            remove = input("Please Choose The Item You Want To Remove: ")
+            shopping_list.remove(remove)
+            print(f"{remove} Has Been Removed! ")
+            pass
+        elif choice == '3':
+            print(f"These Are All Items You Have: {shopping_list}")
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
